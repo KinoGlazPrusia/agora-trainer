03b48f4b80b4a7ebb7a513fecfa3dca0c2b8ff0f
KinoGlazPrusia <jmartinbolet@gmail.com>
2024-10-28 16:52:01 +0100

    Add script list component import to the entry point

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
 import Popup from "../src/components/popup/popup.mjs"
 import Script from "../src/components/script/script.mjs"
+import ScriptList from "../src/components/scriptList/scriptList.mjs"
 import Searchbar from "../src/components/searchbar/searchbar.mjs"
