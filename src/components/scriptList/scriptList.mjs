002f3130fd57eb1ee65a96a7891dea38827de28b
KinoGlazPrusia <jmartinbolet@gmail.com>
2024-10-28 16:54:21 +0100

    Create script list component

---
+++ src/components/scriptList/scriptList.mjs
@@ -0,0 +1,42 @@
+import { PlainComponent, PlainState } from "../../../node_modules/plain-reactive/src/index.js"
+
+class ScriptList extends PlainComponent {
+    constructor() {
+        super('script-list-component', '../src/components/scriptList/scriptList.css')
+
+        this.scripts = new PlainState([], this)
+        this.filter = new PlainState('', this)
+    }
+
+    template() {
+        return `
+            ${this.filteredScripts().map(script => {
+                return script.outerHTML
+            }).join('\n')}
+        `
+    }
+
+    setFilter(filter) {
+        this.filter.setState(filter)
+    }
+
+    clearFilter() {
+        this.filter.setState('')
+    }
+
+    filteredScripts() {
+        if (this.filter.getState().length === 0) return this.scripts.getState()
+        
+        // Implementar la lógica de filtrado
+        const scripts = this.scripts.getState()
+        const filtered = scripts.filter(script => {
+            const data = JSON.parse(script.dataset.script)
+            const regex = new RegExp(this.filter.getState(), 'i')
+            return data.name.match(regex)
+        })
+
+        return filtered
+    }
+}
+
+export default window.customElements.define('script-list-component', ScriptList)
